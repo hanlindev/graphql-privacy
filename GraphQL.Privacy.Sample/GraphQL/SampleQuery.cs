332201f5b7b882eaef61bf601c8e84f68c597bbc
Han Lin <hanlin.dev@gmail.com>
2018-12-24 05:05:21 +0800

    Save project changes

--- GraphQL.Privacy.Sample/GraphQL/SampleQuery.cs
+++ GraphQL.Privacy.Sample/GraphQL/SampleQuery.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using GraphQL.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GraphQL.Privacy.Sample.GraphQL
 {
-    public class SampleQuery
+    public class SampleQuery : ObjectGraphType
     {
+        public SampleQuery()
+        {
+            Name = "Query";
+            Field<UserQuery>("user", resolve: context => new { });
+        }
     }
 }
